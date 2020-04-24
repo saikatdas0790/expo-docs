8d32a150024e7fb2a949a3e53c9eef29480c2d67
Saikat Das <saikat@theknottytales.com>
2020-04-24 14:36:21 +0530

    adding an image

--- image-share/App.js
+++ image-share/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#888", fontSize: 18 }}>
+      <Image
+        source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        style={styles.logo}
+      ></Image>
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -18,5 +23,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
