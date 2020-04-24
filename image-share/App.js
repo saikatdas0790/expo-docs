1b31acbcbe1dfecd41c596b36aae18d40d9001b9
Saikat Das <saikat@theknottytales.com>
2020-04-24 17:33:52 +0530

    picking an image

--- image-share/App.js
+++ image-share/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import logo from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  return (
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) return;
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  return selectedImage ? (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: selectedImage.localUri }}
+        style={styles.thumbnail}
+      ></Image>
+    </View>
+  ) : (
     <View style={styles.container}>
       <Image
         source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
@@ -13,10 +37,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
-      <TouchableOpacity
-        onPress={() => alert("Hello, world!")}
-        style={styles.button}
-      >
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
     </View>
@@ -42,4 +63,9 @@ const styles = StyleSheet.create({
   },
   button: { backgroundColor: "blue", padding: 20, borderRadius: 5 },
   buttonText: { fontSize: 20, color: "#fff" },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
 });
